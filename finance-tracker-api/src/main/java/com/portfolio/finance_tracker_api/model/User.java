53a2b9adeae9bfce3dc783814b221c5778d3cc7d
Ashlin Abraham <ashlin.abraham03@gmail.com>
2025-10-15 21:52:22 +0200

    Implemented user repository and table created.

---
+++ finance-tracker-api/src/main/java/com/portfolio/finance_tracker_api/model/User.java
@@ -0,0 +1,23 @@
+package com.portfolio.finance_tracker_api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
